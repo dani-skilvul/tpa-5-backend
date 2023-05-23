b06ceb94a34cc5d299a067fbf737322730e94a0f
unknown <dainnaxjakarta91@gmail.com>
2023-05-23 13:27:56 +0700

    daf

--- controllers/userController.js
+++ controllers/userController.js
@@ -49,7 +49,7 @@ const registerUserController = async (req, res) => {
       },
     });
   } catch (error) {
-    return res.status(500).json(error);
+    return res.status(500).json({ message: error.message });
   }
 };
 
