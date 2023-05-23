cbc9a704dbeabb31987787242de38a7fe00e734c
unknown <dainnaxjakarta91@gmail.com>
2023-05-23 16:50:03 +0700

    menambahkan token jwt pada login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const User = require("../models").User;
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const registerUserController = async (req, res) => {
   try {
@@ -63,7 +64,60 @@ const registerUserController = async (req, res) => {
   }
 };
 
-const loginUserController = (req, res) => {};
+const loginUserController = async (req, res) => {
+  try {
+    // mengambil data dari req.body
+    const { email, password } = req.body;
+
+    // validasi: jika user tidak / kurang mengirimkan data
+    if (!email || !password) {
+      return res.status(400).json({
+        status: "error",
+        message: "Harap untuk memasukkan email dan password",
+      });
+    }
+
+    // validasi: check user ada atau tidak
+    const user = await User.findOne({
+      where: {
+        email,
+      },
+    });
+    if (!user) {
+      return res.status(404).json({
+        status: "error",
+        message: "Email salah",
+      });
+    }
+
+    // validasi: password benar atau tidak
+    const cekPassword = await bcrypt.compare(password, user.password);
+    if (!cekPassword) {
+      return res.status(401).json({
+        status: "error",
+        message: "Password salah",
+      });
+    }
+
+    // membuat token jwt
+    const token = jwt.sign(
+      { email, password },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: "15s",
+      }
+    );
+
+    // berikan response success
+    return res.json({
+      status: "success",
+      message: "Login berhasil",
+      token,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 
 module.exports = {
   registerUserController,
